edd5a724b52a3c5c4a88bb4a402f5cb2d99ead08
Divine <chuksbon6@gmail.com>
2021-04-16 10:24:48 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const childSection = document.getElementById('child-section');
+
+window.addEventListener('scroll', slideChild)
+
+slideChild()
+
+function slideChild(){
+    console.log(window.innerHeight)
+    const triggerPoint = window.innerHeight / 5 * 4
+    
+}
